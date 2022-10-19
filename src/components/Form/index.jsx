e502e65b3e50fe8c53ce4ce71a3e8ec49ded50d7
daviddubliakov <dubliakov1@gmail.com>
2022-10-19 15:03:37 +0300

    use send data

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './styles.css';
 import { useTelegram } from "../../hooks/useTelegram";
 
@@ -8,21 +8,23 @@ const Form = () => {
   const [subject, setSubject] = useState('physical');
   const { tg } = useTelegram();
 
-  // const onSendData = useCallback(() => {
-  //     const data = {
-  //         country,
-  //         street,
-  //         subject
-  //     }
-  //     tg.sendData(JSON.stringify(data));
-  // }, [country, street, subject])
+  const onSendData = useCallback(() => {
+    const data = {
+      country,
+      street,
+      subject,
+    }
 
-  // useEffect(() => {
-  //     tg.onEvent('mainButtonClicked', onSendData)
-  //     return () => {
-  //         tg.offEvent('mainButtonClicked', onSendData)
-  //     }
-  // }, [onSendData])
+    tg.sendData(JSON.stringify(data))
+  }, [])
+
+  useEffect(() => {
+    tg.WebApp.onEvant('mainButtonClicked', onSendData)
+
+    return () => {
+      tg.WebApp.offEvent('mainButtonClicked', onSendData)
+    }
+  }, [])
 
   useEffect(() => {
     tg.MainButton.setParams({
